2176b7279b154ec278ba52e31326ebb50dcbea8f
Benoît Legat <benoit.legat@gmail.com>
2017-07-13 19:23:12 +0200

    Fix div

--- src/div.jl
+++ src/div.jl
@@ -1,5 +1,5 @@
 # _div(a, b) assumes that b divides a
-function _div(m1::Monomial{true}, m2::Monomial{true})
+function MP._div(m1::Monomial{true}, m2::Monomial{true})
     w, updatez = multdivmono(m1.vars, m2, -)
     Monomial{true}(w, updatez(m1.z))
 end
