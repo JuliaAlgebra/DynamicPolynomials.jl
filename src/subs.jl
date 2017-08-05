f004fd4e302223b7618a62c1a36df7c2e120c6bd
Benoît Legat <benoit.legat@gmail.com>
2017-08-05 16:27:44 +0200

    Update substitution

--- src/subs.jl
+++ src/subs.jl
@@ -1,46 +1,59 @@
-# eval replace all variables by a new value and subs replace some of them.
-# we use different function for that because of type inferability.
-# with eval, Julia knows that all PolyVar will be replaced by values so it can do better inference.
-export subs
-
-function fillmap!(vals, vars, x, varorder)
-    for (i, var) in enumerate(varorder)
-        j = findfirst(vars, var)
-        # If i == 0, that means that the variable is not present
-        # so it is ignored
-        if j > 0
-          vals[j] = x[i]
-        end
+function fillmap!(vals, vars, s::MP.Substitution)
+    j = findfirst(vars, s.first)
+    # If j == 0, that means that the variable is not present
+    # so it is ignored
+    if j > 0
+      vals[j] = s.second
+    end
+end
+function fillmap!(vals, vars, s::MP.AbstractMultiSubstitution)
+    for (v, x) in zip(s.first, s.second)
+        fillmap!(vals, vars, v => x)
     end
 end
 
-function evalmap{C, T}(vars, x::Vector{T}, varorder::Vector{PolyVar{C}})
-    if vars == varorder
-        x
+function fillmap!(vals, vars, s1::MP.AbstractSubstitution, s2::MP.AbstractSubstitution...)
+    fillmap!(vals, vars, s1)
+    fillmap!(vals, vars, s2...)
+end
+
+_eltype{T}(::T) = T
+_eltype(t::Tuple) = Base.promote_typeof(t...)
+_eltype{T}(::Tuple{Vararg{T}}) = T
+_eltype{T}(::AbstractVector{T}) = T
+_substype(s::MP.AbstractSubstitution) = _eltype(s.second)
+_substype(s1::MP.AbstractSubstitution, s2::MP.AbstractSubstitution...) = promote_type(_substype(s1), _substype(s2...))
+_substype(s::MP.Substitutions) = _substype(s...)
+
+function _subsmap(::MP.Eval, vars, s::MP.Substitutions)
+    # Every variable will be replaced by some value of type T
+    vals = Vector{_substype(s)}(length(vars))
+    fillmap!(vals, vars, s...)
+    for i in 1:length(vals)
+        @assert isassigned(vals, i) "Variable $(vars[i]) was not assigned a value"
+    end
+    vals
+end
+function _subsmap{C}(::MP.Subs, vars::Vector{PolyVar{C}}, s::MP.Substitutions)
+    # Some variable may not be replaced
+    vals = Vector{promote_type(_substype(s), PolyVar{C})}(length(vars))
+    copy!(vals, vars)
+    fillmap!(vals, vars, s...)
+    vals
+end
+
+subsmap(st, vars, s::MP.Substitutions) = _subsmap(st, vars, s)
+_vec(a::AbstractVector) = a
+_vec(a::Tuple) = [a...]
+function subsmap(st, vars, s::Tuple{MP.VectorMultiSubstitution})
+    if vars == s[1].first # shortcut
+        _vec(s[1].second)
     else
-        # Every variable will be replaced by some value of type T
-        vals = Vector{T}(length(vars))
-        fillmap!(vals, vars, x, varorder)
-        for i in 1:length(vals)
-            @assert isassigned(vals, i) "Variable $(vars[i]) was not assigned a value"
-        end
-        vals
+        _subsmap(st, vars, s)
     end
 end
 
-function subsmap{C, T}(vars, x::Vector{T}, varorder::Vector{PolyVar{C}})
-    if vars == varorder
-        x
-    else
-        # Some variable may not be replaced
-        vals = Vector{promote_type(T, PolyVar{C})}(length(vars))
-        copy!(vals, vars)
-        fillmap!(vals, vars, x, varorder)
-        vals
-    end
-end
-
-function monoeval(z::Vector{Int}, vals::Vector)
+function monoeval(z::Vector{Int}, vals::AbstractVector)
     @assert length(z) == length(vals)
     @assert !isempty(z)
     val = vals[1]^z[1]
@@ -52,42 +65,33 @@ function monoeval(z::Vector{Int}, vals::Vector)
     val
 end
 
-function (m::PolyVar)(x::Vector, varorder)
-    Monomial(m)(x, varorder)
+_subs(st, ::PolyVar, vals) = monoeval([1], vals::AbstractVector)
+_subs(st, m::Monomial, vals) = monoeval(m.z, vals::AbstractVector)
+_subs(st, t::Term, vals) = t.α * monoeval(t.x.z, vals::AbstractVector)
+function _subs{C, T, S}(::MP.Eval, p::Polynomial{C, T}, vals::AbstractVector{S})
+    # I need to check for iszero otherwise I get : ArgumentError: reducing over an empty collection is not allowed
+    if iszero(p)
+        zero(Base.promote_op(*, S, T))
+    else
+        sum(i -> p.a[i] * monoeval(p.x.Z[i], vals), 1:length(p))
+    end
 end
-
-function (m::Monomial)(x::Vector, varorder)
-    vals = evalmap(_vars(m), x, varorder)
-    monoeval(m.z, vals)
-end
-
-function (t::Term)(x::Vector, varorder)
-    vals = evalmap(_vars(t), x, varorder)
-    t.α * monoeval(t.x.z, vals)
-end
-
-function (p::Polynomial{C, T}){C, T, S}(x::Vector{S}, varorder)
-    vals = evalmap(_vars(p), x, varorder)
-    # I need to check for izero otherwise I get : ArgumentError: reducing over an empty collection is not allowed
-    iszero(p) ? zero(Base.promote_op(*, S, T)) : sum(i -> p.a[i] * monoeval(p.x.Z[i], vals), 1:length(p))
-end
-
-function subs{C, T, S}(p::Polynomial{C, T}, x::Vector{S}, varorder)
-    Tin = S <: PolyType ? S : eltype(S)
-    Tout = Base.promote_op(*, T, Tin)
-    vals = subsmap(_vars(p), x, varorder)
-    # I need to check for izero otherwise I get : ArgumentError: reducing over an empty collection is not allowed
+function _subs{C, T, S}(::MP.Subs, p::Polynomial{C, T}, vals::AbstractVector{S})
+    Tout = Base.promote_op(*, T, MP.coefficienttype(S))
+    # I need to check for iszero otherwise I get : ArgumentError: reducing over an empty collection is not allowed
     if iszero(p)
         zero(Polynomial{C, Tout})
     else
         Polynomial{C, Tout}(sum(i -> p.a[i] * monoeval(p.x.Z[i], vals), 1:length(p)))
     end
+
 end
 
-(p::MatPolynomial)(x::Vector, varorder) = Polynomial(p)(x, varorder)
-subs(p::MatPolynomial, x::Vector, varorder) = subs(Polynomial(p), x, varorder)
-
-function (q::RationalPoly)(x::Vector, varorder)
-    q.num(x, varorder) / q.den(x, varorder)
+function MP.substitute(st::MP.AbstractSubstitutionType, p::PolyType, s::MP.Substitutions)
+    _subs(st, p, subsmap(st, _vars(p), s))
 end
-subs(q::RationalPoly, x::Vector, varorder) = subs(q.num, x, varorder) / subs(q.den, x, varorder)
+
+(v::PolyVar)(s::MP.AbstractSubstitution...)    = MP.substitute(MP.Eval(), v, s)
+(m::Monomial)(s::MP.AbstractSubstitution...)   = MP.substitute(MP.Eval(), m, s)
+(t::Term)(s::MP.AbstractSubstitution...)       = MP.substitute(MP.Eval(), t, s)
+(p::Polynomial)(s::MP.AbstractSubstitution...) = MP.substitute(MP.Eval(), p, s)
