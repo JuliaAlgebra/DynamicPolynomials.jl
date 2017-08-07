1de12429d73e945e79db6bb193efd2d8a0550222
Benoît Legat <benoit.legat@gmail.com>
2017-08-07 16:33:49 +0200

    Improve show

--- src/show.jl
+++ src/show.jl
@@ -1,25 +1,3 @@
-function show(io::IO, m::Monomial)
-    if isconstant(m)
-        show(io, 1)
-    else
-        needsep = false
-        for i in 1:nvars(m)
-            if m.z[i] > 0
-                if needsep
-                    print(io, '*')
-                end
-                show(io, m.vars[i])
-                if m.z[i] > 1
-                    print(io, '^')
-                    print(io, m.z[i])
-                else
-                    needsep = true
-                end
-            end
-        end
-    end
-end
-
 function show(io::IO, x::MonomialVector)
     print(io, typeof(x))
     print(io, "[ ")
