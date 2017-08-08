73f8ffa278e25ae651a72305a2594fefbabce7ef
Benoît Legat <benoit.legat@gmail.com>
2017-08-08 16:53:10 +0200

    Add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,4 +5,16 @@ using Base.Test
 include("mono.jl")
 include("poly.jl")
 
+module newmodule
+    using Base.Test
+    import DynamicPolynomials
+    @testset "Polyvar macro hygiene" begin
+        # Verify that the @polyvar macro works when the package has been activated
+        # with `import` instead of `using`.
+        DynamicPolynomials.@polyvar x y
+        @test isa(x, DynamicPolynomials.PolyVar)
+        @test isa(y, DynamicPolynomials.PolyVar)
+    end
+end
+
 include("mvp.jl")
