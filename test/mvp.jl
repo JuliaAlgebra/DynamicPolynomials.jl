312554961dba6a2408efcd1c821c2c782c76b37e
Benoît Legat <benoit.legat@gmail.com>
2017-08-30 15:13:39 +0200

    Add noncommutative tests

--- test/mvp.jl
+++ test/mvp.jl
@@ -2,3 +2,4 @@ const mvp_test = joinpath(Pkg.dir("MultivariatePolynomials"), "test")
 const Mod = DynamicPolynomials
 include(joinpath(mvp_test, "utils.jl"))
 include(joinpath(mvp_test, "commutativetests.jl"))
+include(joinpath(mvp_test, "noncommutativetests.jl"))
