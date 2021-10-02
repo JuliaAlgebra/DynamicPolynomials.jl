8258ad7d07981e40c0f643fd2f129ba07c8a019d
Benoît Legat <benoit.legat@gmail.com>
2021-10-02 11:14:52 -0400

    Drop variables not in any substitution

--- src/subs.jl
+++ src/subs.jl
@@ -94,7 +94,7 @@ function _subs(::MP.Eval, p::Polynomial{C, T}, vals::AbstractVector{S}) where {C
 end
 function _subs(::MP.Subs, p::Polynomial{C, T}, vals::AbstractVector{S}) where {C, T, S}
     Tout = MA.promote_operation(*, T, MP.coefficienttype(S))
-    q = zero_with_variables(Polynomial{C, Tout}, variables(p))
+    q = zero_with_variables(Polynomial{C, Tout}, mergevars_of(PolyVar{C}, vals)[1])
     for i in 1:length(p.a)
         MA.mutable_operate!(+, q, p.a[i] * monoeval(p.x.Z[i], vals))
     end
