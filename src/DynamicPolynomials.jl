6f63963b0218ef4637557ea90d28cb6923b279c9
Benoît Legat <benoit.legat@gmail.com>
2026-04-17 13:16:18 +0200

    Remove Polynomials

--- src/DynamicPolynomials.jl
+++ src/DynamicPolynomials.jl
@@ -8,82 +8,97 @@ import MutableArithmetics as MA
 import StarAlgebras as SA
 
 include("var.jl")
-#const CommutativeVariable{O,M} = Variable{Commutative{O},M}
-#const NonCommutativeVariable{O,M} = Variable{NonCommutative{O},M}
 include("mono.jl")
 const DMonomialLike{V,M} = Union{Monomial{V,M},Variable{V,M}}
 MA.mutability(::Type{<:Monomial{<:Commutative}}) = MA.IsMutable()
 MA.mutability(::Type{<:Monomial{<:NonCommutative}}) = MA.IsNotMutable()
+
+# Term type comes from MP (SA.Term)
 const _Term{V,M,T} = MP.Term{T,Monomial{V,M}}
-function __add_variables!(t::_Term, allvars, map)
-    return __add_variables!(MP.monomial(t), allvars, map)
-end
+
 include("monomial_vector.jl")
-include("poly.jl")
-MA.mutability(::Type{<:Polynomial}) = MA.IsMutable()
-const TermPoly{V,M,T} = Union{_Term{V,M,T},Polynomial{V,M,T}}
-const PolyType{V,M} =
-    Union{Polynomial{V,M},_Term{V,M},Monomial{V,M},Variable{V,M}}
+
 function MP.variable_union_type(
-    ::Union{PolyType{V,M},Type{<:PolyType{V,M}}},
+    ::Union{DMonomialLike{V,M},Type{<:DMonomialLike{V,M}}},
 ) where {V,M}
     return Variable{V,M}
 end
-MP.constant_monomial(::Type{<:PolyType{V,M}}) where {V,M} = Monomial{V,M}()
-function MP.constant_monomial(p::PolyType)
+function MP.variable_union_type(
+    ::Union{_Term{V,M},Type{<:_Term{V,M}}},
+) where {V,M}
+    return Variable{V,M}
+end
+MP.constant_monomial(::Type{<:DMonomialLike{V,M}}) where {V,M} = Monomial{V,M}()
+function MP.constant_monomial(p::DMonomialLike)
     return Monomial(copy(MP.variables(p)), zeros(Int, nvariables(p)))
 end
-MP.monomial_type(::Type{<:PolyType{V,M}}) where {V,M} = Monomial{V,M}
-MP.monomial_type(::PolyType{V,M}) where {V,M} = Monomial{V,M}
-MP.ordering(p::PolyType) = MP.ordering(MP.variable_union_type(p))
-#function MP.constant_monomial(::Type{Monomial{V,M}}, vars=Variable{V,M}[]) where {V,M}
-#    return Monomial{V,M}(vars, zeros(Int, length(vars)))
-#end
+MP.monomial_type(::Type{<:DMonomialLike{V,M}}) where {V,M} = Monomial{V,M}
+MP.monomial_type(::DMonomialLike{V,M}) where {V,M} = Monomial{V,M}
+MP.monomial_type(::Type{<:_Term{V,M}}) where {V,M} = Monomial{V,M}
+MP.monomial_type(::_Term{V,M}) where {V,M} = Monomial{V,M}
+MP.ordering(p::DMonomialLike) = MP.ordering(MP.variable_union_type(p))
+
 function MP.term_type(
-    ::Union{TermPoly{V,M,T},Type{<:TermPoly{V,M,T}}},
-) where {V,M,T}
-    return _Term{V,M,T}
-end
-function MP.term_type(
-    ::Union{PolyType{V,M},Type{<:PolyType{V,M}}},
+    ::Union{DMonomialLike{V,M},Type{<:DMonomialLike{V,M}}},
     ::Type{T},
 ) where {V,M,T}
     return _Term{V,M,T}
 end
-MP.term_type(::Type{Polynomial{V,M}}) where {V,M} = _Term{V,M}
-MP.polynomial_type(::Type{_Term{V,M}}) where {V,M} = Polynomial{V,M}
-MP.polynomial_type(::Type{_Term{V,M,T}}) where {T,V,M} = Polynomial{V,M,T}
+
+# polynomial_type returns AlgebraElement type
+# We compute the FullBasis type directly to avoid recursion through _polynomial_type
+function _dp_full_basis_type(::Type{Monomial{V,M}}) where {V,M}
+    Vars = Vector{Variable{V,M}}
+    E = Vector{Int}
+    P = MP.Polynomial{MP.Monomial,Vars,E}
+    O = MP.ordering(Variable{V,M})
+    return SA.MappedBasis{
+        P, E,
+        MP.ExponentsIterator{O,Nothing,E},
+        MP.Variables{MP.Monomial,Vars},
+        typeof(MP.exponents),
+    }
+end
+
 function MP.polynomial_type(
-    ::Union{PolyType{V,M},Type{<:PolyType{V,M}}},
+    ::Union{DMonomialLike{V,M},Type{<:DMonomialLike{V,M}}},
     ::Type{T},
 ) where {V,M,T}
-    return Polynomial{V,M,T}
+    return MP.algebra_element_type(Vector{T}, _dp_full_basis_type(Monomial{V,M}))
 end
-MP.variables(p::AbstractArray{<:PolyType}) = mergevars(MP.variables.(p))[1]
+function MP.polynomial_type(::Type{_Term{V,M,T}}) where {V,M,T}
+    return MP.algebra_element_type(Vector{T}, _dp_full_basis_type(Monomial{V,M}))
+end
+function MP.polynomial_type(::Type{_Term{V,M}}) where {V,M}
+    return MP.algebra_element_type(Vector{Any}, _dp_full_basis_type(Monomial{V,M}))
+end
+
+MP.variables(p::AbstractArray{<:DMonomialLike}) = mergevars(MP.variables.(p))[1]
 function MP.nvariables(
-    p::Union{PolyType,MonomialVector,AbstractArray{<:PolyType}},
+    p::Union{DMonomialLike,MonomialVector,AbstractArray{<:DMonomialLike}},
 )
     return length(MP.variables(p))
 end
 function MP.similar_variable(
-    P::Union{PolyType{V,M},Type{<:PolyType{V,M}}},
+    P::Union{DMonomialLike{V,M},Type{<:DMonomialLike{V,M}}},
     ::Type{Val{S}},
 ) where {V,M,S}
     return MP.similar_variable(P, S)
 end
-function MP.similar_variable(p::PolyType{V,M}, s::Symbol) where {V,M}
+function MP.similar_variable(p::DMonomialLike{V,M}, s::Symbol) where {V,M}
     return Variable(string(s), V, M, isreal(p) ? REAL : COMPLEX)
 end
-function MP.similar_variable(::Type{<:PolyType{V,M}}, s::Symbol) where {V,M}
-    return Variable(string(s), V, M, REAL) # we cannot infer this from the type
+function MP.similar_variable(::Type{<:DMonomialLike{V,M}}, s::Symbol) where {V,M}
+    return Variable(string(s), V, M, REAL)
 end
 
 include("promote.jl")
 
-include("operators.jl")
+# Monomial multiplication (cmult.jl and ncmult.jl)
+include("mult.jl")
+
 include("comp.jl")
 
-include("anti_diff.jl")
 include("diff.jl")
 include("subs.jl")
 
--- src/comp.jl
+++ src/comp.jl
@@ -6,7 +6,6 @@ function Base.:(==)(x::Vector{Variable{V,M}}, y::Vector{Variable{V,M}}) where {V
     if length(x) != length(y)
         false
     else
-        #for (xi, yi) in zip(x, y)
         for i in 1:length(x)
             if x[i] != y[i]
                 return false
@@ -41,8 +40,6 @@ function (==)(x::MonomialVector{V,M}, y::MonomialVector{V,M}) where {V,M}
         return false
     end
     allvars, maps = mergevars([MP.variables(x), MP.variables(y)])
-    # Should be sorted in the same order since the non-common
-    # polyvar should have exponent 0
     for (a, b) in zip(x.Z, y.Z)
         A = zeros(Int, length(allvars))
         B = zeros(Int, length(allvars))
@@ -56,56 +53,3 @@ function (==)(x::MonomialVector{V,M}, y::MonomialVector{V,M}) where {V,M}
 end
 (==)(mv::AbstractVector, x::MonomialVector) = monomial_vector(mv) == x
 (==)(x::MonomialVector, mv::AbstractVector) = x == monomial_vector(mv)
-
-# Comparison of Term
-function _compare(p::Polynomial{V,M}, q::Polynomial{V,M}, comparator) where {V,M}
-    # terms should be sorted and without zeros
-    if MP.nterms(p) != MP.nterms(q)
-        return false
-    end
-    for i in eachindex(p.a)
-        if !comparator(p.x[i], q.x[i])
-            # There should not be zero terms
-            @assert p.a[i] != 0
-            @assert q.a[i] != 0
-            return false
-        end
-        if !comparator(p.a[i], q.a[i])
-            return false
-        end
-    end
-    return true
-end
-
-(==)(p::Polynomial{V, M}, q::Polynomial{V, M}) where {V, M} = _compare(p, q, (==))
-Base.isequal(p::Polynomial{V, M}, q::Polynomial{V, M}) where {V, M} = _compare(p, q, isequal)
-
-function Base.isapprox(
-    p::Polynomial{V,M,S},
-    q::Polynomial{V,M,T};
-    rtol::Real = Base.rtoldefault(S, T, 0),
-    atol::Real = 0,
-    ztol::Real = iszero(atol) ? Base.rtoldefault(S, T, 0) : atol,
-) where {V,M,S,T}
-    i = j = 1
-    while i <= length(p.x) || j <= length(q.x)
-        if i > length(p.x) || (j <= length(q.x) && q.x[j] < p.x[i])
-            if !isapproxzero(q.a[j], ztol = ztol)
-                return false
-            end
-            j += 1
-        elseif j > length(q.x) || p.x[i] < q.x[j]
-            if !isapproxzero(p.a[i], ztol = ztol)
-                return false
-            end
-            i += 1
-        else
-            if !isapprox(p.a[i], q.a[j], rtol = rtol, atol = atol)
-                return false
-            end
-            i += 1
-            j += 1
-        end
-    end
-    return true
-end
